e43b5fb3d0c22644c81d7771163e5fcf948fd680
ur13l <ur13l.infante@gmail.com>
2016-06-10 18:19:34 -0500

    Cambios de notificaciones

--- app/src/main/java/code/guanajuato/gob/mx/activatecode/notifications/FirebaseMessagingService.java
+++ app/src/main/java/code/guanajuato/gob/mx/activatecode/notifications/FirebaseMessagingService.java
@@ -3,6 +3,7 @@ package code.guanajuato.gob.mx.activatecode.notifications;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -19,11 +20,10 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         //super.onMessageReceived(remoteMessage);
-        showNotification(remoteMessage.getData().get("message"));
-        Log.d("NOTIFICATION",remoteMessage.getData().get("message"));
+        showNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("message"));
     }
 
-    public void showNotification(String message){
+    public void showNotification(String title, String message){
         Log.d("NOTIFICATION",message);
         Intent i = new Intent(this, HomeActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -34,6 +34,8 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                 .setContentTitle("WOW")
                 .setContentText(message)
                 .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setVibrate(new long[]{200, 1000, 200, 1000, 200})
                 .setContentIntent(pendingIntent);
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         manager.notify(0, builder.build());
