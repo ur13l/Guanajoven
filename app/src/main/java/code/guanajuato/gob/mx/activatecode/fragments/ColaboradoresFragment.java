906f251e96875cc92d4877c9a5bb0c74472fe1e9
ur13l <ur13l.infante@gmail.com>
2016-07-08 21:35:31 -0500

    Cambio en sección de colaboradores CODE e ITL | Íconos en NavigationDrawer

--- app/src/main/java/code/guanajuato/gob/mx/activatecode/fragments/ColaboradoresFragment.java
+++ app/src/main/java/code/guanajuato/gob/mx/activatecode/fragments/ColaboradoresFragment.java
@@ -3,6 +3,9 @@ package code.guanajuato.gob.mx.activatecode.fragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +17,26 @@ import code.guanajuato.gob.mx.activatecode.R;
  * Created by Uriel on 01/02/2016.
  */
 public class ColaboradoresFragment extends CustomFragment {
-    private Button btnColaboradores;
+    private CardView itlCard;
+    private CardView codeCard;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_colaboradores, parent, false);
-        btnColaboradores = (Button) v.findViewById(R.id.btn_colaboradores_code);
-        btnColaboradores.setOnClickListener(new View.OnClickListener() {
+        codeCard = (CardView) v.findViewById(R.id.card_code);
+        codeCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirColaboradores();
+            }
+        });
+
+        itlCard = (CardView) v.findViewById(R.id.card_itl);
+        itlCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirITL();
             }
         });
 
@@ -34,5 +47,14 @@ public class ColaboradoresFragment extends CustomFragment {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse("http://www.codegto.gob.mx/index.php/directorio/"));
         startActivity(i);
+    }
+
+    public void abrirITL(){
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        DetalleColaboradoresFragment f = new DetalleColaboradoresFragment();
+        ft.replace(R.id.segunda_fragment_container, f)
+                .addToBackStack(null)
+                .commit();
     }
 }
