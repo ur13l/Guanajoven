eb07f51c5a8ed3d7a2e81f21d8aff7300a077593
ur13l <ur13l.infante@gmail.com>
2017-02-01 13:24:47 -0600

    Cambios de nuevas interfaces

--- app/src/main/java/mx/gob/jovenes/guanajuato/fragments/MisEventosFragment.java
+++ app/src/main/java/mx/gob/jovenes/guanajuato/fragments/MisEventosFragment.java
@@ -2,6 +2,7 @@ package mx.gob.jovenes.guanajuato.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,23 @@ import mx.gob.jovenes.guanajuato.R;
 /**
  * Created by code on 31/01/17.
  */
-public class MisEventosFragment extends CustomFragment {
+public class MisEventosFragment extends CustomFragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_mis_eventos, container, false);
+        v.findViewById(R.id.ev1).setOnClickListener(this);
+        v.findViewById(R.id.ev2).setOnClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        DetalleEventoFragment f = DetalleEventoFragment.newInstance();
+        ft.replace(R.id.segunda_fragment_container, f).commit();
+
     }
 }
