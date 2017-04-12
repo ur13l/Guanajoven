069afc04d0190de433ad1b530ae9fa6ba17d30c9
Uriel <ur13l.infante@gmail.com>
2017-04-12 12:18:54 -0500

    Cambios de publicidad

---
+++ app/src/main/java/mx/gob/jovenes/guanajuato/api/PublicidadAPI.java
@@ -0,0 +1,18 @@
+package mx.gob.jovenes.guanajuato.api;
+
+import java.util.ArrayList;
+
+import mx.gob.jovenes.guanajuato.model.Publicidad;
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by Uriel on 10/04/2017.
+ */
+
+public interface PublicidadAPI {
+
+    @POST("publicidad/get")
+    Call<Response<ArrayList<Publicidad>>> get();
+}
---
+++ app/src/main/java/mx/gob/jovenes/guanajuato/utils/ImageHandler.java
@@ -0,0 +1,20 @@
+package mx.gob.jovenes.guanajuato.utils;
+
+import java.util.ArrayList;
+
+import mx.gob.jovenes.guanajuato.model.Publicidad;
+
+/**
+ * Created by Uriel on 10/04/2017.
+ */
+
+public class ImageHandler {
+
+    public ImageHandler() {
+
+    }
+
+    public static void start(ArrayList<Publicidad> lista, ViewGrop panel) {
+
+    }
+}
